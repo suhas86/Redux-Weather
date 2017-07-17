b8f2f26e2d3dce6350ed8a485c4d523e50ff6772
suhas <suhas.m@bvbi-infotech.com>
2017-07-17 16:54:47 +0530

    Included Redux Middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = "a27d9ceacd52f69abfffd5de4acaaaf3";
+
+export const FETCH_WEATHER='FETCH_WEATHER';
+
+export function fetchWeather(){
+    return {
+        type:FETCH_WEATHER
+    };
+}
